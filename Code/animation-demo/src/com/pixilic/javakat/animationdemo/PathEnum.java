207a0cc59ea9682e5253dee9f691e7c7a21e778b
Hunter Fuller <hfuller@pixilic.com>
2012-06-23 22:36:29 -0500

    added full 4 paths, fixed ghosting, committing the enum I forgot before

---
+++ Code/animation-demo/src/com/pixilic/javakat/animationdemo/PathEnum.java
@@ -0,0 +1,10 @@
+package com.pixilic.javakat.animationdemo;
+
+public class PathEnum {
+	public enum PathName {
+		RUN_UP, RUN_RIGHT, RUN_DOWN, RUN_LEFT
+	}
+	public enum SpriteName {
+		FACE_UP, FACE_RIGHT, FACE_DOWN, FACE_LEFT
+	}
+}

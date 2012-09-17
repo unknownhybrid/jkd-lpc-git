b73ce5780c174ab7780ff203e08d72ac92e04e64
m <unknownhybrid@gmail.com>
2012-09-17 14:47:18 -0500

    Continued working on Childhood Sounds (Torvald). Haven't touched the sound engine part. Need a health bar image and some single-color squares maybe for testing. Once the "flip through and select" part and the "health calc" part are both complete, all that will be left is art, music, and putting the sound in there. Also, figuring out how to call something "well-timed" with our shitty fuck of an input engine (totally my fault)

---
+++ Code/Memento/src/com/pixilic/javakat/torvaldChildhoodSounds/SoundChunk.java
@@ -0,0 +1,11 @@
+package com.pixilic.javakat.torvaldChildhoodSounds;
+
+import java.awt.image.BufferedImage;
+
+public abstract class SoundChunk {
+	//some sound thing, all of these have images
+	BufferedImage img;
+	public BufferedImage getImage(){
+		return img;
+	}
+}

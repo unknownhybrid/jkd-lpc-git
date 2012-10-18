12809781773d9cadaa47dd69fa90da742f6982ad
m <unknownhybrid@gmail.com>
2012-10-17 21:24:20 -0500

    Started sound engine work, sound work on TorvaldChildhoodSounds

--- Code/Memento/src/com/pixilic/javakat/framework/SFXen.java
+++ Code/Memento/src/com/pixilic/javakat/framework/SFXen.java
@@ -1,5 +1,27 @@
 package com.pixilic.javakat.framework;
 
-public class SFXen {
+import java.io.File;
+import java.io.IOException;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class SFXen {
+	AudioInputStream ais;
+	Clip clip;
+	
+	public SFXen(String filename){
+		try{
+			ais = AudioSystem.getAudioInputStream(new File(filename));
+		}
+		catch(IOException e){
+			System.err.println("Error in: SFXen -- SFXen(). Could not load file (filename: " + filename + ").");
+			System.exit(0);
+		}
+		catch(UnsupportedAudioFileException e){
+			System.err.println("Error in SFXen -- SFXen(). File type ");
+		}
+	}
 }
